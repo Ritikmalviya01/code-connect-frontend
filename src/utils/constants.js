b790e5e61c4aa11071578232c8faac1111be02e4
ritikmalviya40@gmail.com <ritikmalviya40@gmail.com>
2025-04-19 21:18:58 +0530

    BASEURL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,2 @@
-export const BASE_URL = 
-location.hostname === "localhost" ? "http://localhost:3000" : "https://code-connect-backend-production.up.railway.app"
+export const BASE_URL = "https://code-connect-backend-production.up.railway.app"
 
